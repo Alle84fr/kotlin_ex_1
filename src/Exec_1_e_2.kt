65038fb81f714837a7d4f6841f9f109abbfc1844
Alle84fr <arfurlanetto@gmail.com>
2025-03-03 21:11:24 -0300

    correção class e add fun litrao | main arrumararp rintl, var

--- src/Exec_1_e_2.kt
+++ src/Exec_1_e_2.kt
@@ -1,17 +1,30 @@
 import java.util.Scanner
 
-class Pintura (altura:Double, largura: Double){
+class Pintura (val altura:Double, val largura: Double){
+
+    // no parâmetro do class deve por a mtabilidade da variável, seu nome e :tipo
 
     val litro = 2.0
 
-    fun area(faltura:Double, flargura: Double): Double{
-        var area = faltura * flargura
-        return area
+    fun area(): Double{
+
+        // a variável já foi adicinada na parte de cima, então é só chamar-las
+        // deve por o tipo de saída da função
+
+        return altura * largura
+
     }
 
-    fun quantTinta (farea:Double): Double{
-        var tinta = farea / 2
+    fun quantTinta (): Double{
+        var tinta = area() / litro
+        // lembrar que para chamar função deve por ()
         return tinta
+    }
+
+    fun litrao(): Double {
+        val umLitro = 35.30/5.75
+        var xLitros = quantTinta() * umLitro
+        return xLitros
     }
 }
 
@@ -25,7 +38,6 @@ fun conversao(freal:Float): Double {
 }
 
 
-
 fun main() {
     println("Saluton Komencanto")
 
@@ -35,7 +47,19 @@ fun main() {
     println("quantos reais você tem em sua carteira? ")
     real = scanner.nextFloat()
 
-    var altura: Float
-    var largura: Float
+   // ao procurar meu erro vi que posso por direto a variável no scanner
 
+    println("Qual altura da parede? ")
+    var altura = scanner.nextDouble()
+
+    println("Qual lagura da parede? ")
+    var largura = scanner.nextDouble()
+
+    var parede = Pintura(altura, largura)
+
+    var conv = conversao(real)
+
+
+    println("Cliente X,  para pintar ${parede.area()} m² usa-se ${parede.quantTinta()} L tinta" +
+    "\n Se cliente X tem ${real} ele tem ${"%.2f".format(conv)} dolares precisa de $ ${"%2f".format(parede.litrao())}")
 }
